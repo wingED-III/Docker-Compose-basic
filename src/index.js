dde7c4290ddefd59363993af1db793b8606fad2c
WingWM <60010106@kmitl.ac.th>
2021-06-03 00:01:53 +0700

    push to repo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const app = require('express')();
+var path = require('path');
+var cowsay = require("cowsay");
 
-app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is easy 🐳' }) 
+var cow_msg = cowsay.say({
+    text: process.env.cowsay_msg || "Hello",
+    e: "oO",
+    T: "U "
+})
+
+app.get('/', (req, res) =>
+    // res.json({ message: cow_msg })
+    res.send('<pre>' + cow_msg + '</pre>')
 );
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
+app.listen(port, () => {
+    console.log(`app listening on http://localhost:${port}`)
+    console.log(cow_msg);
+});
